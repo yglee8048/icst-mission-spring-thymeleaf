449f0620beacea317ffed7dcce7baafb7ad92b3c
yglee8048 <yglee8048@gmail.com>
2022-02-08 07:34:27 +0900

    fisrt

---
+++ src/main/java/com/lgcns/icst/mission/spring/thymeleaf/hangma/HangmaApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.icst.mission.spring.thymeleaf.hangma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lgcns/icst/mission/spring/thymeleaf/hangma/HangmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.icst.mission.spring.thymeleaf.hangma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
