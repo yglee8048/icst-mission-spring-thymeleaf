4bcba3dc23affe380232eb95d075fb2e6680c3ef
yglee_lgcns <yglee.lgcns@gmail.com>
2022-02-08 10:23:45 +0900

    # COM

--- src/main/java/com/lgcns/icst/mission/spring/thymeleaf/hangma/common/config/FilterConfig.java
+++ src/main/java/com/lgcns/icst/mission/spring/thymeleaf/hangma/common/config/FilterConfig.java
@@ -8,15 +8,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfig {
 
-//    @Bean   // 실제 등록할 때는 주석을 해제
-//    public FilterRegistrationBean<SampleFilter> loggingFilter() {
-//        FilterRegistrationBean<SampleFilter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
-//        filterFilterRegistrationBean.setFilter(new SampleFilter());
-//        filterFilterRegistrationBean.setOrder(1);
-//        filterFilterRegistrationBean.addUrlPatterns("*");
-//        return filterFilterRegistrationBean;
-//    }
-
     @Bean
     public FilterRegistrationBean<LoggingFilter> loggingFilter() {
         FilterRegistrationBean<LoggingFilter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
