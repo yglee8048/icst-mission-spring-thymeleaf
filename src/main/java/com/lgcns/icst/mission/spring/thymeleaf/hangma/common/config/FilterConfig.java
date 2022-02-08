4232cfd987195f4dece7d1b012f5fafac7d84259
yglee8048 <yglee8048@gmail.com>
2022-02-08 10:27:23 +0900

    # 제공 파일 준비

--- src/main/java/com/lgcns/icst/mission/spring/thymeleaf/hangma/common/config/FilterConfig.java
+++ src/main/java/com/lgcns/icst/mission/spring/thymeleaf/hangma/common/config/FilterConfig.java
@@ -1,21 +1,16 @@
 package com.lgcns.icst.mission.spring.thymeleaf.hangma.common.config;
 
-import com.lgcns.icst.mission.spring.thymeleaf.hangma.common.filter.SampleFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.servlet.DispatcherType;
 
 @Configuration
 public class FilterConfig {
 
-//    @Bean   // 실제 등록할 때는 주석을 해제
-    public FilterRegistrationBean<SampleFilter> loggingFilter() {
-        FilterRegistrationBean<SampleFilter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
-        filterFilterRegistrationBean.setFilter(new SampleFilter());
-        filterFilterRegistrationBean.setOrder(1);
-        filterFilterRegistrationBean.addUrlPatterns("*");
-        return filterFilterRegistrationBean;
-    }
+//    @Bean
+//    public FilterRegistrationBean<SampleFilter> sampleFilter() {
+//        FilterRegistrationBean<SampleFilter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+//        filterFilterRegistrationBean.setFilter(new SampleFilter());
+//        filterFilterRegistrationBean.setOrder(1);
+//        filterFilterRegistrationBean.addUrlPatterns("*");
+//        return filterFilterRegistrationBean;
+//    }
 }
